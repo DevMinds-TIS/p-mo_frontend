7aef8aa4a6ff38d154d530d8b3f95d2aeaee2491
--global <1abelpacheco9@gmail.com>
2024-09-11 00:40:32 -0400

    HU3 y correccio de errores

--- src/app/componentes/home/page.tsx
+++ src/app/componentes/home/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from 'next/image';
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import "./home.css";
 
 export default function PruevaPage() {
@@ -14,10 +15,29 @@ export default function PruevaPage() {
     { nombre: "Proyecto 5", id: "a5" },
     { nombre: "Proyecto 6", id: "a6" }
   ]);
+
+  const router = useRouter(); 
+  const [isMounted, setIsMounted] = useState(false); // Verificar si el componente está montado en el cliente
+
+  useEffect(() => {
+    setIsMounted(true); // Indicar que el componente está montado
+  }, []);
   
-  const handleClick = (index) => {
+  const handleClick = (index, num) => {
     setSelected(index);
     console.log(`Botón ${index} presionado`);
+    
+    if (num === 6) { // Revisar si el botón es el 5
+      if (isMounted) {
+        router.push('/'); // Redirige a la ruta deseada
+      }
+    }
+
+    if (num === 5) { // Revisar si el botón es el 5
+      if (isMounted) {
+        router.push('/componentes/editarperfil'); // Redirige a la ruta deseada
+      }
+    }
   };
 
   const handleAgregarProyecto = () => {
@@ -49,18 +69,18 @@ export default function PruevaPage() {
         <div className='imagen'>
           <a href='/componentes/home'>
             <Image
-              src="/iconos/logo.png"
+              src="/iconos/logomenu.svg"
               alt="Logo de la aplicación"
               width={40}
               height={50}
             />
           </a>
         </div>
-        {[1, 2, 3, 4, 5].map((num, index) => (
+        {[1, 2, 3, 4, 5, 6].map((num, index) => (
           <button
             key={index}
             className={`menu-button ${selected === index ? 'active' : ''}`}
-            onClick={() => handleClick(index)}
+            onClick={() => handleClick(index, num)}
           >
             <Image
               src={`/iconos/icon${num}.svg`} 
--- src/app/componentes/registrarequipo/page.tsx
+++ src/app/componentes/registrarequipo/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from 'next/image';
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import "./registrarequipo.css";
 
 export default function PruevaPage() {
@@ -13,11 +14,29 @@ export default function PruevaPage() {
     { nombre: "Equipo 4", id: "e4" }
   ]);
   const [miembros, setMiembros] = useState([{ email: "", rol: "miembro" }]);
+  const router = useRouter(); 
+  const [isMounted, setIsMounted] = useState(false); // Verificar si el componente está montado en el cliente
+
+  useEffect(() => {
+    setIsMounted(true); // Indicar que el componente está montado
+  }, []);
 
   // Acción de los botones en la parte izquierda
-  const handleClick = (index) => {
+  const handleClick = (index, num) => {
     setSelected(index);
     console.log(`Botón ${index} presionado`);
+    
+    if (num === 6) { // Revisar si el botón es el 5
+      if (isMounted) {
+        router.push('/'); // Redirige a la ruta deseada
+      }
+    }
+
+    if (num === 5) { // Revisar si el botón es el 5
+      if (isMounted) {
+        router.push('/componentes/editarperfil'); // Redirige a la ruta deseada
+      }
+    }
   };
 
   const handleRegistrarEquipo = () => {
@@ -52,18 +71,18 @@ export default function PruevaPage() {
         <div className='imagen'>
           <a href='/componentes/home'>
             <Image
-              src="/iconos/logo.png"
+              src="/iconos/logomenu.svg"
               alt="Logo de la aplicación"
               width={40}
               height={50}
             />
           </a>
         </div>
-        {[1, 2, 3, 4, 5].map((num, index) => (
+        {[1, 2, 3, 4, 5, 6].map((num, index) => (
           <button
             key={index}
             className={`menu-button ${selected === index ? 'active' : ''}`}
-            onClick={() => handleClick(index)}
+            onClick={() => handleClick(index, num)}
           >
             <Image
               src={`/iconos/icon${num}.svg`} 
@@ -79,65 +98,67 @@ export default function PruevaPage() {
         <main className="registrarequipos-container">
           <h2>Registrar Equipo</h2>
           <form className="form-container">
-            <div className="datos">
-              {/* Espacio para introducir una imagen */}
-              <label htmlFor="imagen">Subir Imagen:</label>
-              <input type="file" id="imagen" name="imagen" accept="image/*" />
-              
-              {/* Inputs para nombres */}
-              <label htmlFor="nombreEquipo">Nombre del Equipo:</label>
-              <input type="text" id="nombreEquipo" name="nombreEquipo" placeholder="Nombre del equipo" />
-              
-              <label htmlFor="descripcionEquipo">Descripción del Equipo:</label>
-              <input type="text" id="descripcionEquipo" name="descripcionEquipo" placeholder="Descripción del equipo" />
-            </div>
+            <div className='form'>
+              <div className="datos">
+                {/* Espacio para introducir una imagen */}
+                <label htmlFor="imagen">Subir Logo:</label>
+                <input type="file" id="imagen" name="imagen" accept="image/*" />
+                
+                {/* Inputs para nombres */}
+                <label htmlFor="nombreEquipo">Nombre del Equipo:</label>
+                <input type="text" id="nombreEquipo" name="nombreEquipo" placeholder="Nombre del equipo" />
+                
+                <label htmlFor="descripcionEquipo">Nombre largo del equipo:</label>
+                <input type="text" id="descripcionEquipo" name="descripcionEquipo" placeholder="Nombre largo del equipo" />
+              </div>
 
-            <div className="miembros">
-              <h3>Miembros del Equipo</h3>
-              {miembros.map((miembro, index) => (
-                <div key={index} className="miembro-item">
-                  <label htmlFor={`email-${index}`}>Correo del Miembro:</label>
-                  <input
-                    type="email"
-                    id={`email-${index}`}
-                    name={`email-${index}`}
-                    placeholder="Correo del miembro"
-                    value={miembro.email}
-                    onChange={(e) => handleMiembroChange(index, 'email', e.target.value)}
-                  />
+              <div className="miembros">
+                <h3>Miembros del Equipo</h3>
+                {miembros.map((miembro, index) => (
+                  <div key={index} className="miembro-item">
+                    
+                    <input
+                      type="email"
+                      id={`email-${index}`}
+                      name={`email-${index}`}
+                      placeholder="Correo del miembro"
+                      value={miembro.email}
+                      onChange={(e) => handleMiembroChange(index, 'email', e.target.value)}
+                    />
 
-                  <label htmlFor={`rol-${index}`}>Rol:</label>
-                  <select
-                    id={`rol-${index}`}
-                    name={`rol-${index}`}
-                    value={miembro.rol}
-                    onChange={(e) => handleMiembroChange(index, 'rol', e.target.value)}
-                  >
-                    <option value="miembro">Miembro</option>
-                    <option value="productOwner">Product Owner</option>
-                    <option value="scrumMaster">Scrum Master</option>
-                  </select>
-
-                  {/* Botón para eliminar miembro */}
-                  {miembros.length > 1 && (
-                    <button
-                      type="button"
-                      onClick={() => handleEliminarMiembro(index)}
-                      style={{ marginTop: "10px", backgroundColor: "red", color: "white" }}
+                    <select
+                      id={`rol-${index}`}
+                      name={`rol-${index}`}
+                      value={miembro.rol}
+                      onChange={(e) => handleMiembroChange(index, 'rol', e.target.value)}
                     >
-                      Eliminar Miembro
-                    </button>
-                  )}
-                </div>
-              ))}
+                      <option value="miembro">Miembro</option>
+                      <option value="productOwner">Product Owner</option>
+                      <option value="scrumMaster">Scrum Master</option>
+                    </select>
 
-              {/* Botón para agregar nuevo miembro */}
-              {miembros.length < 6 && (
-                <button type="button" onClick={handleAgregarMiembro} style={{ marginTop: "10px" }}>
-                  Agregar Miembro
-                </button>
-              )}
+                    {/* Botón para eliminar miembro */}
+                    {miembros.length > 1 && (
+                      <button
+                        type="button"
+                        onClick={() => handleEliminarMiembro(index)}
+                        style={{backgroundColor: "red", color: "white" }}
+                      >
+                        ✖
+                      </button>
+                    )}
+                  </div>
+                ))}
+
+                {/* Botón para agregar nuevo miembro */}
+                {miembros.length < 6 && (
+                  <button type="button" onClick={handleAgregarMiembro} style={{ marginTop: "10px" }}>
+                    Agregar Miembro
+                  </button>
+                )}
+              </div>
             </div>
+            <button type="submit">Registrar</button>
           </form>
 
           {/* Botón de cancelar */}
