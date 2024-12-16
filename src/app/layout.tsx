b5b0c78a301c26d466849bb50e68366de9def289
Sisa-E <171995044+Sisa-E@users.noreply.github.com>
2024-12-16 17:07:02 -0400

    Se implementaron los contextos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,30 @@
-import { amiko } from "./fonts";
 import "./globals.css";
-import { Providers } from "./provider";
+import { amiko } from "./fonts";
 import type { Metadata } from 'next'
- 
+import { Providers } from "./provider";
+import { AuthProvider } from "@/contexts/AuthContext";
+import { AlertProvider } from "@/contexts/AlertContext";
+import FooterComponent from "./_lib/components/footBar/FooterComponent";
+import NavBarComponent from "./_lib/components/topBar/NavbarComponent";
+
 export const metadata: Metadata = {
   title: 'P-MO',
 }
 
-export default function RootLayout(
-  { children }: { children: React.ReactNode; }) {
+export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
     <html lang='es'>
-      <body className={`${amiko.className} antialiased `}>
+      <body className={`${amiko.className} antialiased`}>
         <Providers>
-          {children}
+          <AuthProvider>
+            <AlertProvider>
+              <NavBarComponent />
+              <section>
+                {children}
+              </section>
+              <FooterComponent />
+            </AlertProvider>
+          </AuthProvider>
         </Providers>
       </body>
     </html>
