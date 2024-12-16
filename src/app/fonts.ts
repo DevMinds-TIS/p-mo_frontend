4c2314acc85b01b757e8e25c808b881a57b466df
Sisa-E <171995044+Sisa-E@users.noreply.github.com>
2024-12-16 17:05:17 -0400

    se agrgaron nuevas fuentes

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,3 +1,6 @@
-import { Amiko } from "next/font/google"
+import { Amiko, Bricolage_Grotesque, Inria_Serif } from "next/font/google"
+
 
 export const amiko = Amiko({subsets: ['latin'], weight: ['400', '600', '700']})
+export const bricolage = Bricolage_Grotesque({subsets: ['latin'], weight: ['200', '300', '400', '500', '600', '700', '800']})
+export const inria = Inria_Serif({subsets: ['latin'], weight: ['300', '400', '700'], style: ['italic', 'normal']})
